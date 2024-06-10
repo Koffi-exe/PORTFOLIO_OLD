ae67177c4d6ac96aa93652724753f575ae6e7fa6
Koffi <124451886+Koffi-exe@users.noreply.github.com>
2024-06-10 13:00:52 +0530

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ import { IoIosMailUnread } from "react-icons/io";
 const Navbar = () => {
   return (
     <nav className="w-full py-5 px-6 mb-12 flex justify-between items-center">
-      <div className="logo text-slate-500 text-2xl font-black">LOGO</div>
+      <div className="logo text-slate-500 text-2xl font-black">As</div>
       <div className="links text-slate-500 flex gap-5 text-2xl">
-        <a href="https://www.linkedin.com/in/adithya-sasi-5ba33b1aa/">{<FaLinkedin />}</a>
+        <a href="https://www.linkedin.com/in/adithya-sasi-5ba33b1aa/" target="_blank" >{<FaLinkedin />}</a>
         <a href="https://github.com/Koffi-exe/">{<FaGithub />}</a>
         <a href="mailto:adithyaaa@outlook.com">{<IoIosMailUnread />}</a>
         {/* <a href="#">{<FaSquareXTwitter />}</a> */}
